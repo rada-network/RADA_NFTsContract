36ee0b196bb2326214785f7d6c080fa864eb2aa2
Quang Dinh <ngocquangbb@gmail.com>
2022-01-10 16:26:50 +0700

    set minter

--- scripts/RadaNftContract/3_setMintFactories.js
+++ scripts/RadaNftContract/3_setMintFactories.js
@@ -19,9 +19,9 @@ async function main() {
   // await nftContract.setMintFactory("0x7C2e7fF40254c465d1941B923C669Df9F6488898"); // Auction
   // await nftContract.setMintFactory("0x49117A8E02872C0B6b399829823bD912Bf74B097"); // Fixed
   // await nftContract.setMintFactory("0x2da63e30F846E2Dc9bF7A6982E8e7F805C89b67b"); // Open Box
-  await nftContract.setMintFactory("0x0E0989d3aA04bA61292A6eAfD283bC860b7208b3"); // Open Box
-  // await nftContract.setMintFactory("0x64951b9C752897Aeaf5e85d7c22Fb07DB3309766"); // Auction
-  // await nftContract.setMintFactory("0xB94f9f965Fdd16c409226B6A70d25A34D2bB7f76"); // Fixed
+  await nftContract.setMintFactory("0xF567103C1F846217C58995c205662F3ECF22302B"); // Open Box
+  await nftContract.setMintFactory("0x63B70bAD9a1958eF743EeF22086637FaD92f3555"); // Auction
+  await nftContract.setMintFactory("0x8cE3514596bfe2E15c1e38c3446DCca17F2317df"); // Fixed
 
 
   console.log("setMintFactory changed");
